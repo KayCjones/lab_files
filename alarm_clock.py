afe336e9227ebd23c448076807cc1faef0cec06c
Kasey Jones <kaseyjones@Kaseys-MBP.lan>
2022-05-11 15:39:20 -0500

    Created alarm clock class with first function for alarm clock lab

---
+++ alarm_clock.py
@@ -0,0 +1,8 @@
+
+class Alarm_Clock:                                                                      # no parenthesis needed when creating classes 
+    def __init__(self):                                                                   # name of the class needs to be uppercase 
+        self.time = "12:00_pm"                                                           # every function involving classes needs to have (self) as a perameter 
+        self.change_time = "12:00_am"
+        self.on_off = False
+    
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from alarm_clock import Alarm_Clock
+
+test_alarm_clock = Alarm_Clock()
+print(test_alarm_clock.time)
